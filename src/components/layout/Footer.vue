efb59fb140565fec88e0185a07896fbb38d8f170
abhinav <ambhinav@yahoo.com>
2020-06-18 01:42:13 +0800

    implemented navbar

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-footer
+    absolute
+    class="font-weight-medium"
+    app
+  >
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>Halale</strong>
+    </v-col>
+  </v-footer>
+</template>
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav>
+    <v-app-bar app color="green">
+      <v-app-bar-nav-icon
+        @click="drawer = true"
+        class="hidden-md-and-up"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <v-btn text to="/">
+          Halale
+        </v-btn>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn icon>
+          <v-icon>mdi-cart</v-icon>
+        </v-btn>
+        <v-btn text  to="/about">
+          Contact us
+        </v-btn>
+      </v-toolbar-items>
+
+    </v-app-bar>
+    <!-- Start of mobile side menu -->
+    <v-navigation-drawer absolute v-model="drawer" left temporary>
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-cart</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Cart</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- End of mobile side menu -->
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data () {
+      return {
+        drawer: false,
+        group: null,
+      }
+    }
+}
+</script>
+
+<style>
+</style>
