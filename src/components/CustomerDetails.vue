36ac112489476f0b5e2af05737870b7529a5af24
abhinav <ambhinav@yahoo.com>
2020-06-24 05:10:41 +0800

    updated customer details form

--- src/components/CustomerDetails.vue
+++ src/components/CustomerDetails.vue
@@ -4,7 +4,7 @@
       class="headline grey lighten-3 justify-center"
       primary-title
     >
-      <h2>Your delivery details:</h2>
+      <h2>Your Delivery Details</h2>
     </v-card-title>
 
     <v-card-text class="px3">
@@ -44,18 +44,35 @@
           </v-row>
           <v-row>
             <v-col cols="12">
-              <v-textarea 
-                label="Special instructions...e.g. Delivery, Food restrictions" 
-                v-model="specialInstructions"
-                ></v-textarea>
+              <v-autocomplete
+                label="Payment Method" 
+                v-model="paymentMethod"
+                :rules="getTextRules"
+                :items="paymentMethods"
+                required
+                ></v-autocomplete>
             </v-col>
           </v-row>
         </v-container>
       </v-form>
+      <v-list-item four-line class="caption font-weight-light">
+        <v-list-item-content>
+          <v-list-item-title>By placing your order you agree to:</v-list-item-title>
+          <v-list-item-subtitle>
+            <a>Data Processing Policy</a>
+          </v-list-item-subtitle>
+          <v-list-item-subtitle>
+            <a>End User License Agreement of Online Ordering System</a>
+          </v-list-item-subtitle>
+          <v-list-item-subtitle>
+            <a>Halale's Terms of Service</a>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item> 
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn :loading="loading" class="primary mx-0 mt-3 mr-3" text @click="handleSubmit">Submit</v-btn>
+      <v-btn :loading="loading" class="primary mx-0 mt-3 mr-3" text @click="handleSubmit">Place order</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -70,8 +87,13 @@ export default {
       customerName: null,
       postalCode: null,
       phoneNumber: null,
-      specialInstructions: null,
-      loading: false
+      loading: false,
+      paymentMethod: null,
+      paymentMethods: [
+        "Cash on delivery",
+        "Paynow",
+        "Stripe",
+      ]
     }
   },
   computed: {
@@ -91,7 +113,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
