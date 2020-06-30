abcde905526d9b285a5bda8c0e30e401130ea0e4
abhinav <ambhinav@yahoo.com>
2020-06-30 21:04:47 +0800

    init telegram bot hanlder

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
+// const admin = require("firebase-admin");
+const Telegraf = require("telegraf");
+// const Markup = require('telegraf/markup')
+const TELEGRAM = require('./secrets/telegram');
+const bot = new Telegraf(TELEGRAM.BOT_TOKEN);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// exports.onOrderAdded = functions.firestore
+//   .document('Orders/{orders}')
+//   .onCreate(async (snap, context) => {
+//     // TODO: format the message, send message to admin with metada of order id
+//   });
+
+/** Bot-related scripts */
+
+// use this method and then set webhook instead of using polling with bot.launch() if there are issues.
+// see https://github.com/telegraf/telegraf/issues/98
+exports.botWebhook = functions.https.onRequest(
+    (req, res) => bot.handleUpdate(req.body, res).then(rv => !rv && res.sendStatus(200))
+)
+
+// middleware
+bot.use(async (ctx, next) => {
+    if (ctx.callbackQuery && ctx.callbackQuery.data) {
+        const callbackData = JSON.parse(ctx.callbackQuery.data);
+        console.log(`Incoming task ${callbackData.cmd} for bot`, ctx.callbackQuery.data)
+    }
+    return next()
+})
