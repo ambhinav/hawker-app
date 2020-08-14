af81dfc0341590992cb2cbf471b9decb08ef8456
abhinav <ambhinav@yahoo.com>
2020-08-14 17:54:54 +0800

    improved checkout error feedback

--- src/views/StoreUser.vue
+++ src/views/StoreUser.vue
@@ -25,135 +25,134 @@
             </h1>
           </v-col>
         </v-row> 
-    </v-img>
-    <v-container>
-      <v-row class="text-center" justify="center">
-        <v-col cols="12" class="mb-4">
-          <h2 class="font-weight-bold">
-            {{ getMarket.name }} shops
-          </h2>
-        </v-col>
-      </v-row>
-      <v-row>
-        <InfoBanner info="Please note that there is a minimum spend of $4 for each shop that you buy from, and $30 overall. Please check out by 5pm" />
-      </v-row>
-      <v-row>
-        <v-col cols="12" :md="getStoresInMarket.length === 1 ? 12 : 6" v-for="(store, i) in getStoresInMarket" :key="i">
-          <v-skeleton-loader
-            v-if="loading"
-            type="card"
-          >
-          </v-skeleton-loader>
-          <Menu v-else :store="store" />
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-bottom-sheet 
-      v-if="isCheckoutButtonShown"
-      v-model="sheet"
-      hide-overlay
-      inset
-      persistent
-      scrollable
-      no-click-animation
-    >
-      <v-card>
-        <v-card-title 
-          style="font-size: 16px; font-family: 'Palanquin Dark', sans-serif;" 
-          v-if="!cartOpen" 
-          class="d-flex justify-space-between"
-          >
-          {{getCartLength}} Items in cart 
-          <v-btn v-if="isCartOpen" @click="toggleCartState" depressed>Open Cart</v-btn>
-        </v-card-title>
-        <v-card-title 
-          style="font-size: 16px; font-family: 'Palanquin Dark', sans-serif;" 
-          v-else class="d-flex justify-space-between"
-          >
-          {{getCartLength}} Items in cart 
-          <v-btn color="error" v-if="isCartOpen" @click="toggleCartState" depressed>Close Cart</v-btn>
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-card-text v-if="cartOpen && isCartOpen">
-          <v-list>
-            <v-list-item-group>
-              <v-list-item 
-              v-for="(item, i) in getCart"
-              :key="i"
-              class="justify-md-space-between justify-sm-space-between"
-              >
-                <v-list-item-content class="hidden-sm-and-down">
-                    <v-avatar size="36px" style="margin-left: 5px;">    
-                        <img v-if="item.image"
-                            :src="item.image"
-                        >
-                    </v-avatar>    
-                </v-list-item-content>
-                <v-list-item-content class="hidden-sm-and-down">
-                    {{item.name}}
-                </v-list-item-content>
-                <v-list-item-content class="hidden-sm-and-down">
-                    ${{parseFloat(item.price).toFixed(2)}}
-                </v-list-item-content>
-                <v-list-item-content class="hidden-sm-and-down">
-                    <v-btn v-if="item.qty > 1" x-small fab dark color="primary" @click="decrementQty(item)">
-                      <v-icon dark>mdi-minus</v-icon>
-                    </v-btn> 
-                    <!-- <v-list-item-title v-text="item.qty"></v-list-item-title> -->
-                    <v-btn text>
-                      <span>{{ item.qty }}</span>
-                    </v-btn>
-                    <v-btn x-small fab dark color="primary" @click="incrementQty(item)">
-                      <v-icon dark>mdi-plus</v-icon>
-                    </v-btn>
-                </v-list-item-content>
-                <v-list-item-content class="hidden-sm-and-down">
-                    <v-btn depressed @click="removeItemFromCart(item)"><v-icon>mdi-delete</v-icon></v-btn>
-                </v-list-item-content>
-                <v-list-item-content class="hidden-md-and-up">
-                  <v-row justify="center">
-                    <v-col cols="4">
+      </v-img>
+      <v-container>
+        <v-row class="text-center" justify="center">
+          <v-col cols="12" class="mb-4">
+            <h2 class="font-weight-bold">
+              {{ getMarket.name }} shops
+            </h2>
+          </v-col>
+        </v-row>
+        <v-row>
+          <InfoBanner info="Please note that there is a minimum spend of $4 for each shop that you buy from, and $30 overall. Please check out by 5pm" />
+        </v-row>
+        <v-row>
+          <v-col cols="12" :md="getStoresInMarket.length === 1 ? 12 : 6" v-for="(store, i) in getStoresInMarket" :key="i">
+            <v-skeleton-loader
+              v-if="loading"
+              type="card"
+            >
+            </v-skeleton-loader>
+            <Menu v-else :store="store" />
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-bottom-sheet 
+        v-model="sheet"
+        hide-overlay
+        inset
+        persistent
+        scrollable
+        no-click-animation
+      >
+        <v-card>
+          <v-card-title 
+            style="font-size: 16px; font-family: 'Palanquin Dark', sans-serif;" 
+            v-if="!cartOpen" 
+            class="d-flex justify-space-between"
+            >
+            {{getCartLength}} Items in cart 
+            <v-btn v-if="isCartOpen" @click="toggleCartState" depressed>Open Cart</v-btn>
+          </v-card-title>
+          <v-card-title 
+            style="font-size: 16px; font-family: 'Palanquin Dark', sans-serif;" 
+            v-else class="d-flex justify-space-between"
+            >
+            {{getCartLength}} Items in cart 
+            <v-btn color="error" v-if="isCartOpen" @click="toggleCartState" depressed>Close Cart</v-btn>
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-card-text v-if="cartOpen && isCartOpen">
+            <v-list>
+              <v-list-item-group>
+                <v-list-item 
+                v-for="(item, i) in getCart"
+                :key="i"
+                class="justify-md-space-between justify-sm-space-between"
+                >
+                  <v-list-item-content class="hidden-sm-and-down">
+                      <v-avatar size="36px" style="margin-left: 5px;">    
+                          <img v-if="item.image"
+                              :src="item.image"
+                          >
+                      </v-avatar>    
+                  </v-list-item-content>
+                  <v-list-item-content class="hidden-sm-and-down">
                       {{item.name}}
-                    </v-col>
-                    <v-col cols="3">
-                      ${{item.price}}
-                    </v-col>
-                    <v-col cols="3">
-                      <v-icon class="pr-2" v-if="item.qty > 1" color="primary" @click="decrementQty(item)" x-small dark>mdi-minus</v-icon>
-                      <span>{{ item.qty }}</span>
-                      <v-icon class="pl-2" color="primary" @click="incrementQty(item)" x-small dark>mdi-plus</v-icon>
-                    </v-col>
-                    <v-col cols="2">
-                      <v-icon small @click="removeItemFromCart(item)">mdi-delete</v-icon>
-                    </v-col>
-                  </v-row>
-                </v-list-item-content>
-              </v-list-item>
-              
-            </v-list-item-group>
-          </v-list>
-        </v-card-text>
-        <v-btn :loading="loading" :disabled="!isValidPurchase" color="primary" @click="checkOut()" style="height: 50px; font-size: 15px;">
-          <span style="font-family: 'Palanquin Dark', sans-serif;">Check Out - </span>
-          <b>${{getTotalPrice}}</b>
-        </v-btn>
-        <!-- <v-card-actions>
-              <div class="error--text subtitle-2 font-weight-light hidden-sm-and-down">*Note that there is a minimum spend of $4 for each shop that you buy from, and $30 overall.</div>
-              <div class="error--text subtitle-2 font-weight-light hidden-md-and-up">*Note: Minimum spend of $4 for each shop purchased from, and $30 overall.</div>
-              <v-spacer></v-spacer>
-              <v-btn bottom :loading="loading" :disabled="!isValidPurchase" color="primary" @click="checkOut()" style="height: 50px; font-size: 15px;">
-                <span style="font-family: 'Palanquin Dark', sans-serif;">Check Out - </span>
-                <b>${{getTotalPrice}}</b>
-              </v-btn>
-        </v-card-actions> -->
-      </v-card>
-    </v-bottom-sheet>
+                  </v-list-item-content>
+                  <v-list-item-content class="hidden-sm-and-down">
+                      ${{parseFloat(item.price).toFixed(2)}}
+                  </v-list-item-content>
+                  <v-list-item-content class="hidden-sm-and-down">
+                      <v-btn v-if="item.qty > 1" x-small fab dark color="primary" @click="decrementQty(item)">
+                        <v-icon dark>mdi-minus</v-icon>
+                      </v-btn> 
+                      <!-- <v-list-item-title v-text="item.qty"></v-list-item-title> -->
+                      <v-btn text>
+                        <span>{{ item.qty }}</span>
+                      </v-btn>
+                      <v-btn x-small fab dark color="primary" @click="incrementQty(item)">
+                        <v-icon dark>mdi-plus</v-icon>
+                      </v-btn>
+                  </v-list-item-content>
+                  <v-list-item-content class="hidden-sm-and-down">
+                      <v-btn depressed @click="removeItemFromCart(item)"><v-icon>mdi-delete</v-icon></v-btn>
+                  </v-list-item-content>
+                  <v-list-item-content class="hidden-md-and-up">
+                    <v-row justify="center">
+                      <v-col cols="4">
+                        {{item.name}}
+                      </v-col>
+                      <v-col cols="3">
+                        ${{item.price}}
+                      </v-col>
+                      <v-col cols="3">
+                        <v-icon class="pr-2" v-if="item.qty > 1" color="primary" @click="decrementQty(item)" x-small dark>mdi-minus</v-icon>
+                        <span>{{ item.qty }}</span>
+                        <v-icon class="pl-2" color="primary" @click="incrementQty(item)" x-small dark>mdi-plus</v-icon>
+                      </v-col>
+                      <v-col cols="2">
+                        <v-icon small @click="removeItemFromCart(item)">mdi-delete</v-icon>
+                      </v-col>
+                    </v-row>
+                  </v-list-item-content>
+                </v-list-item>
+                
+              </v-list-item-group>
+            </v-list>
+          </v-card-text>
+          <v-btn :loading="loading" color="primary" @click="checkOut()" style="height: 50px; font-size: 15px;">
+            <span style="font-family: 'Palanquin Dark', sans-serif;">Check Out - </span>
+            <b>${{getTotalPrice}}</b>
+          </v-btn>
+          <!-- <v-card-actions>
+                <div class="error--text subtitle-2 font-weight-light hidden-sm-and-down">*Note that there is a minimum spend of $4 for each shop that you buy from, and $30 overall.</div>
+                <div class="error--text subtitle-2 font-weight-light hidden-md-and-up">*Note: Minimum spend of $4 for each shop purchased from, and $30 overall.</div>
+                <v-spacer></v-spacer>
+                <v-btn bottom :loading="loading" :disabled="!isValidPurchase" color="primary" @click="checkOut()" style="height: 50px; font-size: 15px;">
+                  <span style="font-family: 'Palanquin Dark', sans-serif;">Check Out - </span>
+                  <b>${{getTotalPrice}}</b>
+                </v-btn>
+          </v-card-actions> -->
+        </v-card>
+      </v-bottom-sheet>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex';
+import { mapGetters, mapMutations, mapActions } from 'vuex';
 import Menu from '@/components/Menu';
 import InfoBanner from '@/components/feedback/InfoBanner';
 import { isSameDay } from '@/utils/dateTimeUtil';
@@ -231,7 +230,7 @@ export default {
       return allStores;
     },
     isValidPurchase() {
-      return this.isCartFilled && (this.getTotalPrice > 30.00)
+      return this.isCartFilled && (this.getTotalPrice >= 30.00)
     },
     isCartOpen() {
       return this.getCart.length > 0;
@@ -246,6 +245,7 @@ export default {
       setDeliveryDetails: 'setDeliveryDetails',
       resetCartState: "resetCartState"
     }),
+    ...mapActions(["successToast", "errorToast"]),
     handlePageReload() {
       console.log("Here")
     },
@@ -253,7 +253,17 @@ export default {
       this.cartOpen = !this.cartOpen
     },
     checkOut() {
-      this.$router.push({ name: "OrderDetails" })
+      if (this.isValidPurchase) {
+        this.$router.push({ name: "OrderDetails" })
+      } else {
+        if (!this.isCartFilled && this.getTotalPrice < 30.0) {
+          return this.errorToast("Please add at least $30 worth of items to cart");
+        } else if (!this.isCartFilled) {
+          return this.errorToast("Please add at least $4 worth of items from each store you purchased from");
+        } else {
+          return this.errorToast("Please add at least $30 worth of items to cart");
+        }
+      }
     },
     setUpComponent() {
       return new Promise(resolve => setTimeout(() => resolve(this.loading = false), 3000));
