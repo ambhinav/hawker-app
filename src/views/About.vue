bdcee3850ab6c20af9edf0a08811f9b453ccd68f
abhinav <ambhinav@yahoo.com>
2020-09-16 17:01:25 +0800

    Updated UI for about us and home page
    Used the new pictures from dropbox, and also made them both mobile
    friendly

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,62 @@
 <template>
-  <div>
-    <v-container>
-      <div class="display-3 text-center pt-10 primary--text">About Foodboys</div>
-      <div class="body-1 pt-5">
+  <v-container>
+    <v-row justify="center">
+      <v-col>
+        <div class="display-3 text-sm-center pt-10 primary--text">About Foodboys</div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <div class="body-1 pa-5">
         Ever felt like eating a combination of hawker delicacies from a particular hawker centre but deterred by the multiple delivery charges? Fret not! With just a few simple clicks, Foodboys will manage your group orders and send them to you, all with ONE delivery fee! The founders at Foodboys aim to deliver the very best of sumptuous Halal hawker food amalgamated from your favourite stalls around the island to your very doorstep. Our business model serves as a bridge to connect halal hawkers who work under the same roof, to their customers. 
         <br>
         <br>
         Foodboys also believe in quality family time, enjoying dinner in the comfort of your home with the whole family truly strengthens family bonding. 
       </div>
-      <div class="display-3 text-center pt-10 primary--text">About the Founders</div>
-      <div class="body-1 pt-5">
-        <b>Ibrahim</b> started selling chendol at Haig Road Market and was roped into this business to help his family at a young age. This has allowed Ibrahim to establish friendly relationships with fellow hawkers. Ibrahim was always looking out for ways to use technology to give the entire community a leverage and push so that more customers can attempt his fellow hawker friends’ dishes!
-        <br>
-        <br>
-        <b>Kiran Prabakaran</b> has a deep passion for entrepreneurship and driving changes. With a heart to help the larger community, Kiran is always on the lookout to streamline existing processes and implement focused driven changes for the hawkers. Using new innovations to achieve a greater good, this has allowed him to improve existing food delivery systems and establish something that no one has ever done in the market! 
-        <br>
-        The founders have put together their collaborative ideas to deliver famous halal food from our heartland neighbourhoods to Singapore’s humble abodes!
-        <br>
-        With Foodboys, you could have your favourite Mee goreng from that stall you frequent, along with putu piring and teh tarik from the uncle in the next corner, all for ONE delivery fee! Order from our website today!  
-      </div>
-    </v-container>
-  </div>
+    </v-row>
+    <v-row>
+      <v-col>
+        <div class="display-3 text-sm-center primary--text">About the Founders</div>
+      </v-col>
+    </v-row>
+    <v-row align="center">
+      <v-col cols="4" md="2">
+        <v-avatar class="profile hidden-sm-and-down" size="164">
+          <v-img src="https://uc836666244f6fc8ff92041537ad.previews.dropboxusercontent.com/p/thumb/AA4lbB2jyQa4RLZNUv96vbMJR8QGb-sa0sVdkrCTzRDqK8Mv3zmrgC3JJc-spXnTZy3gMp4Ak2Cgs2lhxVqblJZNWCuBvJlsPD3-nAKkYIMM755u_Z6JrqM8BTezeoLpLulrAm7bZp0n0kyCn5XBv43fCkWyuYgWSxw5HTdLFcIC6bzZq2gwxZBn0dGDtgDzHOyjpvMxwYYR5rSrj5JWIlayWMu5l02TOOUF7DybVl724wWKo-pVme9Rogd6BtmRJ6psIg3nKoZK4gE5V0pPa6Q51pyBhczoaPmUNXVcmT_2Lvp2iL3fDDZtjcP5vqYXX_RmZ3SHtuKvji6x7-fo8nBRyHBn1TJvFBC4_Iupezx8LpfZy2aFPH-tfttsFaxqsZsZoH5j3wfWBHTm9WGg6UEuEdErlumzDvtU1f8Dlg815faJf0iFmJOpYd2MY2XIs1CdN4ckvoGE3IBjU_Wm1MGW1JB0EvmHsHI3lAeaWy1uWkUwSPBdTY8K_wSHyzVrNIQGexPNuyA3axgz5bYtlB0Qu5D4smq1G7SPxtZibiIwRuv0GMDnO1xE6MdUzzIKbFxVfKbhU4AvbfBapnQ5YUYK/p.jpeg?size=178x178&size_mode=1"></v-img>
+        </v-avatar>
+        <v-avatar class="profile hidden-md-and-up" size="102">
+          <v-img src="https://uc836666244f6fc8ff92041537ad.previews.dropboxusercontent.com/p/thumb/AA4lbB2jyQa4RLZNUv96vbMJR8QGb-sa0sVdkrCTzRDqK8Mv3zmrgC3JJc-spXnTZy3gMp4Ak2Cgs2lhxVqblJZNWCuBvJlsPD3-nAKkYIMM755u_Z6JrqM8BTezeoLpLulrAm7bZp0n0kyCn5XBv43fCkWyuYgWSxw5HTdLFcIC6bzZq2gwxZBn0dGDtgDzHOyjpvMxwYYR5rSrj5JWIlayWMu5l02TOOUF7DybVl724wWKo-pVme9Rogd6BtmRJ6psIg3nKoZK4gE5V0pPa6Q51pyBhczoaPmUNXVcmT_2Lvp2iL3fDDZtjcP5vqYXX_RmZ3SHtuKvji6x7-fo8nBRyHBn1TJvFBC4_Iupezx8LpfZy2aFPH-tfttsFaxqsZsZoH5j3wfWBHTm9WGg6UEuEdErlumzDvtU1f8Dlg815faJf0iFmJOpYd2MY2XIs1CdN4ckvoGE3IBjU_Wm1MGW1JB0EvmHsHI3lAeaWy1uWkUwSPBdTY8K_wSHyzVrNIQGexPNuyA3axgz5bYtlB0Qu5D4smq1G7SPxtZibiIwRuv0GMDnO1xE6MdUzzIKbFxVfKbhU4AvbfBapnQ5YUYK/p.jpeg?size=178x178&size_mode=1"></v-img>
+        </v-avatar>
+      </v-col>
+      <v-col cols="8" md="10">
+        <div class="body-1 pt-5">
+          <b>Ibrahim</b> started selling chendol at Haig Road Market and was roped into this business to help his family at a young age. This has allowed Ibrahim to establish friendly relationships with fellow hawkers. Ibrahim was always looking out for ways to use technology to give the entire community a leverage and push so that more customers can attempt his fellow hawker friends’ dishes!
+          <br>
+          <br>
+        </div>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+    <v-row align="center">
+      <v-col cols="8" md="8" class="pa-5">
+        <div class="body-1">
+          <b>Kiran Prabakaran</b> has a deep passion for entrepreneurship and driving changes. With a heart to help the larger community, Kiran is always on the lookout to streamline existing processes and implement focused driven changes for the hawkers. Using new innovations to achieve a greater good, this has allowed him to improve existing food delivery systems and establish something that no one has ever done in the market! 
+          <br>
+          The founders have put together their collaborative ideas to deliver famous halal food from our heartland neighbourhoods to Singapore’s humble abodes!
+        </div>
+      </v-col>
+      <v-col cols="4" md="4" class="pt-5">
+        <v-avatar class="profile hidden-sm-and-down" size="164">
+          <v-img src="https://uc5305cfc9662fa48cb1d9d112a2.previews.dropboxusercontent.com/p/thumb/AA7MYYlomdnDRFujx0LdS-ELE2BTvbBvboHlaX-_N-E65-udEQyEx9xQtLsHYASXvaHXnNJWjrhbCxsd4uNdx7QGO1mftkNq2zYtuZlH4gNqWhQkjhPHssWlsMRwP6G9aG4iVIrrqc4uY7q7D-sio3iS64UNEjm1eOBJk43N8cwqq-5HZLPGJKdLro8T9VkhK07vIm8bbbWDv1am88y890amtU145jnkZ-J4RjwqhLvFNNOndj0wDFV8ymmOelZ4n7HbdYIBCmOipcYMOj5ryKiLm06V7Vq9aTp1GOgNNFYfQAOV8c7wQXwwIPRBo6VMtpLdmqpNJ4mR-uAeVlrE4ZUCplA2CgJ3vNksKb4pKVXSthpD9owCQn-LnUjTEY7WrCMhTo1SceM2iy2w1WIW_LAjTrHVxWovioLhGrGRMl2zQsbOVhVy1sTOQ3nlIROdI6ohksDvb3M9CLuRQ-fqotSC3FBDizPeeoKsZ6m5UL_-RIy-Dq33p1jXvK-xjaUUBXIGJ-U1QX_IKkgJT1ba7Gf6QnVaBro0biOZ_aUvdHxrA_nkGzk4roHUK0fkXzB6wluhcDGI6wOt9B9XhYkbxxwO/p.jpeg?size=178x178&size_mode=1"></v-img>
+        </v-avatar>
+        <v-avatar class="profile hidden-md-and-up" size="102">
+          <v-img src="https://uc5305cfc9662fa48cb1d9d112a2.previews.dropboxusercontent.com/p/thumb/AA7MYYlomdnDRFujx0LdS-ELE2BTvbBvboHlaX-_N-E65-udEQyEx9xQtLsHYASXvaHXnNJWjrhbCxsd4uNdx7QGO1mftkNq2zYtuZlH4gNqWhQkjhPHssWlsMRwP6G9aG4iVIrrqc4uY7q7D-sio3iS64UNEjm1eOBJk43N8cwqq-5HZLPGJKdLro8T9VkhK07vIm8bbbWDv1am88y890amtU145jnkZ-J4RjwqhLvFNNOndj0wDFV8ymmOelZ4n7HbdYIBCmOipcYMOj5ryKiLm06V7Vq9aTp1GOgNNFYfQAOV8c7wQXwwIPRBo6VMtpLdmqpNJ4mR-uAeVlrE4ZUCplA2CgJ3vNksKb4pKVXSthpD9owCQn-LnUjTEY7WrCMhTo1SceM2iy2w1WIW_LAjTrHVxWovioLhGrGRMl2zQsbOVhVy1sTOQ3nlIROdI6ohksDvb3M9CLuRQ-fqotSC3FBDizPeeoKsZ6m5UL_-RIy-Dq33p1jXvK-xjaUUBXIGJ-U1QX_IKkgJT1ba7Gf6QnVaBro0biOZ_aUvdHxrA_nkGzk4roHUK0fkXzB6wluhcDGI6wOt9B9XhYkbxxwO/p.jpeg?size=178x178&size_mode=1"></v-img>
+        </v-avatar>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <div class="body-1 pa-2">With Foodboys, you could have your favourite Mee goreng from that stall you frequent, along with putu piring and teh tarik from the uncle in the next corner, all for ONE delivery fee! Order from our website today!</div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="home">
     <v-img
-      :src="require('@/assets/home.jpg')"
+      src="https://uc1bf508601f7266dc3861dcdf09.previews.dropboxusercontent.com/p/thumb/AA4S5xd16AcXJoeq6eCDSjcvI2s1g0hRw8kSiRRlmRjwovwNizHX2dpP5kSFhyBbtlEytG6GwbhwTtK7sgh7ZVc7He88U7zq-FoItcr_VZYauXXQ8WnJO1O1ZzumiNpm4KAQcxw6t2HR74-TwJGIhB-oq3xBS2uYz_PWBLVQ9vCLxLA2HvD5io2t-PJn3WE5U_xQ_rVtorX-zJu4lrcg-TqdqUWRRL7neRAuSygAfPfsUggFrKsx6DRFguZBG3vUqyKGGhT9D2ZXCWYYG_IgqWgGNzBZJrNlFSE7xf7UMIBXiCySXh5EII3lFinSOigCLYzDFaPJcfwlr8slVaO9gC2qAwnejHHucbATuRXd0lQv8YWsAi-3fCTmBvSH1F0K6CMC40TsMMCNy5HeNvpN8DDgQlGph6tu6VFZHssGU8Sx1g/p.jpeg?fv_content=true&size_mode=5"
       lazy-src="https://images.unsplash.com/photo-1577303935007-0d306ee638cf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1325&q=80"
       class="align-center hidden-sm-and-down"
       contain
-      aspect-ratio="4:3"
-      style="transform: rotate(-90deg)"
     >
     </v-img> 
     <v-img
-      :src="require('@/assets/home.jpg')"
-      aspect-ratio="0.47"
+      src="https://uc8d5934d20b2e30f9c5deeb9d1b.previews.dropboxusercontent.com/p/thumb/AA6S5He8EKIVnfT4mm3dGIB6hcU_IfiBMa3p7SKV1FKfg4m7ZBmvZ3q6ln5jm_g4vNg_eo_IxwoO2dbgypv1C6pNr2xlceJgjHnyRf7PdCNaH-_EBzRST1tLAWnOdoVnLs61_nncYwFioRdKT3a6179dgx9PNqjprtHY8bv9JeCffmUWV2bX9PHfyZil_cKEkbtdDYoVgMjgNiqRCCNi8zoDHqM70IHP-WU0dzhxum38vre9aJ3KgM_vDuQojYinJvpWfrCmKGIipAfgaFsHwnOi0roLIVgY8Cg0bUDHcWcwmxaE0PCSSMCfD5kh74jIGIfsHls0lG0Ml6XEEC1brj0tiqfB2rF_OEFEPESq1D7VDDknD86YofxVvOF5BB0S7MOLcuDpYOHF0b5E30SUfr-PTXHjBIBt7cJbSMXU1s2FryXg0xn-jpXvhCZZPGFARnO6xm62shKY07DNP62AyN76p3UdmaBENy69mU05N-S2mg/p.jpeg?fv_content=true&size_mode=5"
       lazy-src="https://images.unsplash.com/photo-1577303935007-0d306ee638cf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1325&q=80"
       class="align-center hidden-md-and-up"
+      contain
     >
     </v-img>
       <!-- <v-container fluid>
