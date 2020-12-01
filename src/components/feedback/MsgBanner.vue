c0b9aa925f51dfa0f4f83a56c329b3114be75884
abhinav <ambhinav@yahoo.com>
2020-12-01 11:25:35 +0400

    feat: improve message banners

---
+++ src/components/feedback/MsgBanner.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row class="pa-4 secondary">
+        <ul id="messages-1">
+            <li v-for="(item, index) in items" :key="index">
+                {{ item.message }}
+            </li>
+        </ul>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "MsgBanner",
+  props: ["items"]
+};
+</script>
+
+<style></style>
